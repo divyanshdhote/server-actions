19c494a3311b4b6be39f73c8f3a2b6a9a40be3ac
Divyansh <divyanshdhote77@gmail.com>
2025-10-15 11:53:46 +0530

    auth route added

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,10 +1,12 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { PrismaClient } from "@/generated/prisma";
+import { nextCookies } from "better-auth/next-js";
 
 const prisma = new PrismaClient();
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql", // or "mysql", "postgresql", ...etc
   }),
+  plugins: [nextCookies()],
 });
